2d896de98b00be74964568b4ca8ae4605ce67a2d
SunSki <everyday.natsuki@gmail.com>
2019-02-07 04:53:25 +0900

    first
    最初の投稿

---
+++ app/scripts/contentscript.js
@@ -0,0 +1,27 @@
+'use strict';
+
+$(function () {
+    //jQueryでCSSを変更する
+    $('.main-header').remove();
+    $('header').html('<h1>さむらいえんじにあ!</h1>');
+    $('aside').remove();
+    $('div.header-sticky').remove();
+    $('footer').remove();
+    $('.fade-in').remove();
+    $('.eye-catch-content-wrapper').remove();
+    $('.mbt-border-top').remove();
+    $('.vce-related-box').remove();
+    $('.p-entry_push').remove();
+    $('.wp_social_bookmarking_light').remove();
+    $('blockquote').remove();
+
+    var id = Number.MAX_SAFE_INTEGER;
+    $('#text,span,p,h5,div,ul,a').html(function (index, element) {
+        if (element == 'まとめ') {
+            id = index;
+        }
+        if (index >= id) {
+            return '';
+        }
+    });
+});
